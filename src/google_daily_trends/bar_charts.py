e5a57dc867a1b99d7e174c128d3215de30d6b2b5
Juraj Acsay <juraj.acsay@seznam.cz>
2024-04-28 18:05:31 +0200

    Generating info is finished

--- src/google_daily_trends/bar_charts.py
+++ src/google_daily_trends/bar_charts.py
@@ -1,15 +1,21 @@
 import datetime
+import webbrowser
+import randomcolor
 
 import matplotlib.pyplot as plt
-import randomcolor
+
 
 
 class BarData:
     def __init__(self):
-        pass
+        self.bar_to_url = ""
 
-    @staticmethod
-    def visualize_data(first_list, second_list):
+    def on_pick(self, event):
+        bar = event.artist
+        if bar in self.bar_to_url:
+            webbrowser.open(self.bar_to_url[bar])
+
+    def visualize_data(self, first_list, second_list, third_list):
         fig, ax = plt.subplots(num=f'Trends of {datetime.date.today().strftime("%d %B, %Y")}')  # "%d %B, %Y"
         fix_list = []
         colors = []
@@ -18,11 +24,13 @@ class BarData:
             colors.append(color[0])
         for i in second_list:
             fix_list.append(int(i.split("+")[0]))
-        ax.bar(first_list, fix_list, label=first_list, color=colors)
+        bars = ax.bar(first_list, fix_list, label=first_list, color=colors, picker=5)
+        self.bar_to_url = {bar: url for bar, url in zip(bars, third_list)}
+        fig.canvas.mpl_connect('pick_event', self.on_pick)
         ax.set_ylabel('search numbers')
         ax.set_xlabel('trend names')
         ax.tick_params(axis='y', labelsize=8)
-        ax.tick_params(axis='x', labelsize=6)
+        ax.set_xticklabels([])
         ax.set_title('Daily trends')
         ax.legend(title='Trend Names')
         plt.show()
--- src/google_daily_trends/utils.py
+++ src/google_daily_trends/utils.py
@@ -1,7 +1,10 @@
 import json
-
+import time
 import requests
+
 from selenium import webdriver
+from selenium.common import WebDriverException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 from src.google_daily_trends.trends_locator import Trends
@@ -9,7 +12,11 @@ from src.google_daily_trends.trends_locator import Trends
 
 class Utils(Trends):
     def __init__(self):
-        self.driver = webdriver.Chrome()
+
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument('headless')
+        self.chrome_options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(options=self.chrome_options)
         self.driver.get(self.get_location())
         self.wait = WebDriverWait(self.driver, 10)
         Trends.__init__(self)
@@ -22,7 +29,36 @@ class Utils(Trends):
         return f"https://trends.google.com/trends/trendingsearches/daily?geo={country_code}&hl=cs"
 
     def start_of_the_script(self):
-        self.haha()
+        self.fill_trends()
 
     def driver_close(self):
         self.driver.close()
+
+    def wait_for_element(self, located_by, path, clickable=None, send_text=None, clearable=None, wait_time=None):
+        force_click_element = self.wait.until(EC.presence_of_element_located((located_by, path)))
+        try:
+            unhidden_element = self.wait.until(EC.presence_of_element_located((located_by, path)))
+            if clickable:
+                unhidden_element.click()
+            if clearable:
+                unhidden_element.clear()
+            if send_text:
+                if wait_time:
+                    time.sleep(wait_time)
+                unhidden_element.send_keys(send_text)
+            if wait_time:
+                time.sleep(wait_time)
+        except TimeoutException:
+            time.sleep(2)
+            self.driver.execute_script("arguments[0].click();", force_click_element)
+        except WebDriverException:
+            time.sleep(2)
+            self.driver.execute_script("arguments[0].click();", force_click_element)
+
+    @staticmethod
+    def is_element_present(web_driver, located_by, path, timeout=3):
+        try:
+            WebDriverWait(web_driver, timeout).until(EC.presence_of_element_located((located_by, path)))
+            return True
+        except TimeoutException:
+            return False
